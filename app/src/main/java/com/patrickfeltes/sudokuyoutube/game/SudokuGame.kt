7009e7f5672f714d176e785ac22c6e13f2b14a4f
patrickfeltes <pj6444@gmail.com>
2019-01-23 11:56:56 -0600

    Tutorial 4 Code

--- app/src/main/java/com/patrickfeltes/sudokuyoutube/game/SudokuGame.kt
+++ app/src/main/java/com/patrickfeltes/sudokuyoutube/game/SudokuGame.kt
@@ -1,17 +1,33 @@
 package com.patrickfeltes.sudokuyoutube.game
 
 import android.arch.lifecycle.MutableLiveData
+import android.util.Log
 
 class SudokuGame {
 
     var selectedCellLiveData = MutableLiveData<Pair<Int, Int>>()
+    var cellsLiveData = MutableLiveData<List<Cell>>()
 
     private var selectedRow = -1
     private var selectedCol = -1
 
+    private val board: Board
+
     init {
+        val cells = List(9 * 9) {i -> Cell(i / 9, i % 9, i % 9)}
+        board = Board(9, cells)
+
         selectedCellLiveData.postValue(Pair(selectedRow, selectedCol))
+        cellsLiveData.postValue(board.cells)
     }
+
+    fun handleInput(number: Int) {
+        if (selectedRow == -1 || selectedCol == -1) return
+
+        board.getCell(selectedRow, selectedCol).value = number
+        cellsLiveData.postValue(board.cells)
+    }
+
 
     fun updateSelectedCell(row: Int, col: Int) {
         selectedRow = row
--- app/src/main/java/com/patrickfeltes/sudokuyoutube/view/custom/SudokuBoardView.kt
+++ app/src/main/java/com/patrickfeltes/sudokuyoutube/view/custom/SudokuBoardView.kt
@@ -4,9 +4,11 @@ import android.content.Context
 import android.graphics.Canvas
 import android.graphics.Color
 import android.graphics.Paint
+import android.graphics.Rect
 import android.util.AttributeSet
 import android.view.MotionEvent
 import android.view.View
+import com.patrickfeltes.sudokuyoutube.game.Cell
 
 class SudokuBoardView(context: Context, attributeSet: AttributeSet) : View(context, attributeSet) {
 
@@ -19,6 +21,8 @@ class SudokuBoardView(context: Context, attributeSet: AttributeSet) : View(conte
     private var selectedCol = 0
 
     private var listener: SudokuBoardView.OnTouchListener? = null
+
+    private var cells: List<Cell>? = null
 
     private val thickLinePaint = Paint().apply {
         style = Paint.Style.STROKE
@@ -42,6 +46,12 @@ class SudokuBoardView(context: Context, attributeSet: AttributeSet) : View(conte
         color = Color.parseColor("#efedef")
     }
 
+    private val textPaint = Paint().apply {
+        style = Paint.Style.FILL_AND_STROKE
+        color = Color.BLACK
+        textSize = 24F
+    }
+
     override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
         super.onMeasure(widthMeasureSpec, heightMeasureSpec)
         val sizePixels = Math.min(widthMeasureSpec, heightMeasureSpec)
@@ -52,20 +62,20 @@ class SudokuBoardView(context: Context, attributeSet: AttributeSet) : View(conte
         cellSizePixels = (width / size).toFloat()
         fillCells(canvas)
         drawLines(canvas)
+        drawText(canvas)
     }
 
     private fun fillCells(canvas: Canvas) {
-        if (selectedRow == -1 || selectedCol == -1) return
+        cells?.forEach {
+            val r = it.row
+            val c = it.col
 
-        for (r in 0..size) {
-            for (c in 0..size) {
-                if (r == selectedRow && c == selectedCol) {
-                    fillCell(canvas, r, c, selectedCellPaint)
-                } else if (r == selectedRow || c == selectedCol) {
-                    fillCell(canvas, r, c, conflictingCellPaint)
-                } else if (r / sqrtSize == selectedRow / sqrtSize && c / sqrtSize == selectedCol / sqrtSize) {
-                    fillCell(canvas, r, c, conflictingCellPaint)
-                }
+            if (r == selectedRow && c == selectedCol) {
+                fillCell(canvas, r, c, selectedCellPaint)
+            } else if (r == selectedRow || c == selectedCol) {
+                fillCell(canvas, r, c, conflictingCellPaint)
+            } else if (r / sqrtSize == selectedRow / sqrtSize && c / sqrtSize == selectedCol / sqrtSize) {
+                fillCell(canvas, r, c, conflictingCellPaint)
             }
         }
     }
@@ -101,6 +111,22 @@ class SudokuBoardView(context: Context, attributeSet: AttributeSet) : View(conte
         }
     }
 
+    private fun drawText(canvas: Canvas) {
+        cells?.forEach {
+            val row = it.row
+            val col = it.col
+            val valueString = it.value.toString()
+
+            val textBounds = Rect()
+            textPaint.getTextBounds(valueString, 0, valueString.length, textBounds)
+            val textWidth = textPaint.measureText(valueString)
+            val textHeight = textBounds.height()
+
+            canvas.drawText(valueString, (col * cellSizePixels) + cellSizePixels / 2 - textWidth / 2,
+                    (row * cellSizePixels) + cellSizePixels / 2 - textHeight / 2, textPaint)
+        }
+    }
+
     override fun onTouchEvent(event: MotionEvent): Boolean {
         return when (event.action) {
             MotionEvent.ACTION_DOWN -> {
@@ -120,6 +146,11 @@ class SudokuBoardView(context: Context, attributeSet: AttributeSet) : View(conte
     fun updateSelectedCellUI(row: Int, col: Int) {
         selectedRow = row
         selectedCol = col
+        invalidate()
+    }
+
+    fun updateCells(cells: List<Cell>) {
+        this.cells = cells
         invalidate()
     }
 
